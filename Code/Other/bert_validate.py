2aac2cb868fb077615a3c73121a53818bc2013aa
linkwire <00e5ee@gmail.com>
2021-12-02 12:42:53 -0800

    bert model validation - prelim, untested

---
+++ Code/Other/bert_validate.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+# model
+def build_classifier_model():
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+    preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+    encoder_inputs = preprocessing_layer(text_input)
+    encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+    outputs = encoder(encoder_inputs)
+    net = outputs['pooled_output']
+    net = tf.keras.layers.Dropout(0.1)(net)
+    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+    return tf.keras.Model(text_input, net)
+
+
+classifier_model = build_classifier_model()
+
+# the model path
+saved_model_path = './imdb_bert'
+
+# save the model
+classifier_model.save(saved_model_path, include_optimizer=False)
+
+# load the saved model
+model = tf.saved_model.load(saved_model_path)
+
+
+# validation examples
+examples = [
+    'this is such an amazing movie!',  # this is the same sentence tried earlier
+    'The movie was great!',
+    'The movie was meh.',
+    'The movie was okish.',
+    'The movie was terrible...'
+]
+
+results = tf.sigmoid(model(tf.constant(examples)))
+
+print(results)
