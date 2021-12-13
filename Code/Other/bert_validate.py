0cd5e31dbfbe38fefb3df9a7c60bca07b7f97475
linkwire <00e5ee@gmail.com>
2021-12-13 11:55:25 -0800

    test against custom input

--- Code/Other/bert_validate.py
+++ Code/Other/bert_validate.py
@@ -1,39 +1,8 @@
-import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text
 
-# model
-def build_classifier_model():
-    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-    preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
-    encoder_inputs = preprocessing_layer(text_input)
-    encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
-    outputs = encoder(encoder_inputs)
-    net = outputs['pooled_output']
-    net = tf.keras.layers.Dropout(0.1)(net)
-    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
-    return tf.keras.Model(text_input, net)
+bert_model = hub.load('imdb_electra')
 
-
-classifier_model = build_classifier_model()
-
-# the model path
-saved_model_path = './imdb_bert'
-
-# save the model
-classifier_model.save(saved_model_path, include_optimizer=False)
-
-# load the saved model
-model = tf.saved_model.load(saved_model_path)
-
-
-# validation examples
-examples = [
-    'this is such an amazing movie!',  # this is the same sentence tried earlier
-    'The movie was great!',
-    'The movie was meh.',
-    'The movie was okish.',
-    'The movie was terrible...'
-]
-
-results = tf.sigmoid(model(tf.constant(examples)))
-
-print(results)
+joke = ["This movie was good, Nic Cage was awesome."]
+result = bert_model(joke)
+print(result)
