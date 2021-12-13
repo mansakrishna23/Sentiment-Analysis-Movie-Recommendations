f4e45ecf6aa8bb46cd1fd9ecaec8be1babcc63e7
linkwire <00e5ee@gmail.com>
2021-12-13 14:28:44 -0800

    added model repo

--- Code/BERT/validate.py
+++ Code/BERT/validate.py
@@ -3,6 +3,13 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
 
+# Getting the Models:
+# Unfortunately, GitHub enforces file size limits to be less than 100 MB.
+# Consequently, I am hosting the models and variables on Google Drive as a public folder.
+# Include the folder in the same parent directory that houses this file, and it should run without errors.
+# You can find the files here: https://drive.google.com/drive/folders/1HDwiN1V25_uOkTUApsER5mJCQ7ivOOFe?usp=sharing
+
+
 # Load model of Choice
 electra_base = hub.load('imdb_models\\electra_base')
 
